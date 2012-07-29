ab466e5d1ad7d16c68fefedb8ceea776cfde64d1
elnatnal <jonathanstephens@jonathan-stephenss-imac.local>
2012-07-28 21:09:58 -0400

    minified css

--- wp-content/themes/newdzerk/config.rb
+++ wp-content/themes/newdzerk/config.rb
@@ -7,13 +7,9 @@ sass_dir = "scss"
 images_dir = "imgs"
 javascripts_dir = "js"
 
-# You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
 
-output_style = :expanded 
+output_style = :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 relative_assets = true
-
-# To disable debugging comments that display the original location of your selectors. Uncomment:
 line_comments = false
