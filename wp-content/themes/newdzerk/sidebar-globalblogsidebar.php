999b34dd13163f9f0ccc17011a8755521c5284ea
elnatnal <jonathanstephens@jonathan-stephenss-imac.local>
2012-08-02 01:58:40 -0400

    fixed newsletter styles on global sidebar

--- wp-content/themes/newdzerk/sidebar-globalblogsidebar.php
+++ wp-content/themes/newdzerk/sidebar-globalblogsidebar.php
@@ -2,13 +2,16 @@
        <?php dynamic_sidebar('global-blog-sidebar'); ?>
 
 
-       <div class="newsletter">
+
+       <div class="newsletter-sign-up">
               <h4>Sign up for our newsletter</h4>
               <form action="http://lfov.net/webrecorder/f" method="post" name="Subscribe to Blog">
               <input type="hidden" name="formid" value="25efc106-0e17-4823-b436-8f703ea7b6af"/>
               <input type="hidden" name="cid" value="LF_af4faade"/>
-              <p><label for="email">Email:</label><input type="text" name="email" id="email" /></p>
-              <p class="submit"><input type="submit" name="Add Registration" value="Submit"></p>
+              <p><label for="email">Enter your email</label><br /><input type="text" name="email" id="email" /></p>
+              <button class="submit"><input type="submit" name="Add Registration" value="Submit"></button>
               </form>
-       </div>
+       </div>               
+
+
 </aside>
