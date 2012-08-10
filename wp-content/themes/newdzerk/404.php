ca840aa069fb238d55e4485664e3b661a533a01f
elnatnal <jonathanstephens@jonathan-stephenss-imac.local>
2012-08-09 21:39:33 -0400

    added 404 page, check it on the live site. also, improved no search results view [close #33]

--- wp-content/themes/newdzerk/404.php
+++ wp-content/themes/newdzerk/404.php
@@ -1,7 +1,29 @@
 <?php get_header(); ?>
 
-	<h2><?php _e('Error 404 - Page Not Found','html5reset'); ?></h2>
+<hgroup>
+       <?php $sassy = get_post_meta($post->ID, "adzerk-sassy-text", true); ?>
+       <h1 class="sassytext">
+          <?php _e('Onward to pages that exist because','html5reset'); ?>
+       </h1>
+       <h1>You've found the 404</h1>
+</hgroup>
 
-<?php get_sidebar(); ?>
+<section class="fourohfour">
+       <h3 class="d3-d9 blue3">Aha! Snooping around somewhere that doesn't exist.</h3>
+       <p class="d3-d8">
+              You can stare longingly at the magnificent body that is our berzerker, but that doesn't do much good. We're not entirely sure what you're looking for, but here are a few possibilities:<p>
+              <ul class="d3-d8">
+                     <li><a href="<?php bloginfo('home_url'); ?>">All our blogs</a></li>
+                     <li><a href="<?php bloginfo('home_url'); ?>/news-and-announcements">News and Announcements</a></li>
+                     <li><a href="<?php bloginfo('home_url'); ?>/product-blog">Product Blog</a></li>
+                     <li><a href="<?php bloginfo('home_url'); ?>/team-blog">Team Blog</a></li>
+                     <li><a href="<?php bloginfo('home_url'); ?>/press-and-media">Press Page</a></li>                                              
+              </ul>
+                     <p class="d3-d8"><em>Hope that helps in some way. If not, get on your boat and go back <a href="<?php bloginfo('home_url'); ?>">home.</a> Safe travels on your journey.</em></p>
+
+</section>
+
+
+<?php dynamic_sidebar('pricing-sidebar'); ?>
 
 <?php get_footer(); ?>
--- wp-content/themes/newdzerk/search.php
+++ wp-content/themes/newdzerk/search.php
@@ -30,11 +30,42 @@
 		<?php include (TEMPLATEPATH . '/_/inc/nav.php' ); ?>
 
 	<?php else : ?>
+              <hgroup>
+                     <h1 class="sassytext">
+                        Looking for something?
+                     </h1>
+                     <h1>Sorry, no results.</h1>
+              </hgroup>
+	
+	       <article>
+		       <h2 style="text-align:center;">Try looking at a through our blogs, you might find one that's useful</h2><br />
+                     <div class="inside-adzerk-blog-summary">
+                            <div class="blog-summary blue3">
+                                  <h4>News and Announcements</h4>
+                                  <p>Here are the latest postings about us and what we are doing around the office and world community.&emsp;<a href="news-and-announcements">Read Posts</a></p> 
+                            </div>
 
-		<h2>No posts found.</h2>
+                            <div class="blog-summary blue1">
+                                  <h4>Product Blog</h4>
+                                  <p>We roll out new features like every day here. Look at what's being implemented right now!&emsp;<a href="product-blog">Read More</a></p> 
+                            </div>
+
+                            <div class="blog-summary blue2">
+                                  <h4>Team Blog</h4>
+                                  <p>Here's where we rant, rave and talk about random opportunities we have at work. It's cool. Check it.&emsp;<a href="team-blog">Read Posts</a></p> 
+                            </div>
+
+                            <div class="blog-summary orange">
+                                  <h4>Run of Network</h4>
+                                  <p>Just how do you run an ad network? Check out tips, tricks and wisdom about the ad tech and ad industry.&emsp;<a href="http://runofnetwork.adzerk.com/">Read Posts</a></p> 
+                            </div>
+                     </div>
+		       
+		</article>
 
 	<?php endif; ?>
+       <?php dynamic_sidebar('pricing-sidebar'); ?>
+
 </div> 
-       <?php get_sidebar('globalblogsidebar'); ?>
 
 <?php get_footer(); ?>
