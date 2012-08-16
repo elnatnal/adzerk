eca4a635b5f5003d2b83b4b532a7266d9e285087
elnatnal <jonathanstephens@jonathan-stephenss-imac.local>
2012-08-15 21:12:46 -0400

    fixed some size issues on custom-ados-themes

--- wp-content/themes/newdzerk/page-custom-ados-themes.php
+++ wp-content/themes/newdzerk/page-custom-ados-themes.php
@@ -10,37 +10,37 @@
 
 <div class="custom-ados-themes-container">
        <div class="theme">
-             <a class="custom-theme" href="<?php bloginfo('stylesheet_directory'); ?>/imgs/custom-ados-themes/hotchalk.jpg" title="Hotchalk"><img src="http://adzerk-www.s3.amazonaws.com/marketing_site/themes/hotchalk_sm.jpg"></a>
+             <a class="custom-theme" href="<?php bloginfo('stylesheet_directory'); ?>/imgs/custom-ados-themes/hotchalk.jpg" title="Hotchalk"><img src="<?php bloginfo('stylesheet_directory'); ?>/imgs/custom-ados-themes/hotchalk_sm.jpg"></a>
              <h4>Hotchalk</h4>
        </div>
 
        <div class="theme">
-             <a class="custom-theme" href="<?php bloginfo('stylesheet_directory'); ?>/imgs/custom-ados-themes/adbase.jpg" title="Adbase"><img src="http://adzerk-www.s3.amazonaws.com/marketing_site/themes/adbase_sm.jpg"></a>
+             <a class="custom-theme" href="<?php bloginfo('stylesheet_directory'); ?>/imgs/custom-ados-themes/adbase.jpg" title="Adbase"><img src="<?php bloginfo('stylesheet_directory'); ?>/imgs/custom-ados-themes/adbase_sm.jpg"></a>
              <h4>Adbase</h4>
        </div>
 
        <div class="theme">
-             <a class="custom-theme" href="<?php bloginfo('stylesheet_directory'); ?>/imgs/custom-ados-themes/builtAds.jpg" title="Built Ads"><img src="http://adzerk-www.s3.amazonaws.com/marketing_site/themes/builtAds_sm.jpg"></a>
+             <a class="custom-theme" href="<?php bloginfo('stylesheet_directory'); ?>/imgs/custom-ados-themes/builtAds.jpg" title="Built Ads"><img src="<?php bloginfo('stylesheet_directory'); ?>/imgs/custom-ados-themes/builtAds_sm.jpg"></a>
              <h4>Built Ads</h4>
        </div>
 
        <div class="theme">
-             <a class="custom-theme" href="<?php bloginfo('stylesheet_directory'); ?>/imgs/custom-ados-themes/litbreaker.jpg" title="Litbreaker"><img src="http://adzerk-www.s3.amazonaws.com/marketing_site/themes/litbreaker_sm.jpg"></a>
+             <a class="custom-theme" href="<?php bloginfo('stylesheet_directory'); ?>/imgs/custom-ados-themes/litbreaker.jpg" title="Litbreaker"><img src="<?php bloginfo('stylesheet_directory'); ?>/imgs/custom-ados-themes/litbreaker_sm.jpg"></a>
              <h4>Litbreaker</h4>
        </div>
 
        <div class="theme">
-             <a class="custom-theme" href="<?php bloginfo('stylesheet_directory'); ?>/imgs/custom-ados-themes/messageSpace.jpg" title="Message Space"><img src="http://adzerk-www.s3.amazonaws.com/marketing_site/themes/messageSpace_sm.jpg"></a>
+             <a class="custom-theme" href="<?php bloginfo('stylesheet_directory'); ?>/imgs/custom-ados-themes/messageSpace.jpg" title="Message Space"><img src="<?php bloginfo('stylesheet_directory'); ?>/imgs/custom-ados-themes/messageSpace_sm.jpg"></a>
              <h4>Message Space</h4>
        </div>
 
        <div class="theme">
-             <a class="custom-theme" href="<?php bloginfo('stylesheet_directory'); ?>/imgs/custom-ados-themes/muslimAdNetwork.jpg" title="Muslim Ad Network"><img src="http://adzerk-www.s3.amazonaws.com/marketing_site/themes/muslimAdNetwork_sm.jpg"></a>
+             <a class="custom-theme" href="<?php bloginfo('stylesheet_directory'); ?>/imgs/custom-ados-themes/muslimAdNetwork.jpg" title="Muslim Ad Network"><img src="<?php bloginfo('stylesheet_directory'); ?>/imgs/custom-ados-themes/muslimAdNetwork_sm.jpg"></a>
              <h4>Muslim Ad Network</h4>
        </div>
 
        <div class="theme">
-             <a class="custom-theme" href="<?php bloginfo('stylesheet_directory'); ?>/imgs/custom-ados-themes/netsportsMedia.jpg" title="Net Sports Media"><img src="http://adzerk-www.s3.amazonaws.com/marketing_site/themes/netsportsMedia_sm.jpg"></a>
+             <a class="custom-theme" href="<?php bloginfo('stylesheet_directory'); ?>/imgs/custom-ados-themes/netsportsMedia.jpg" title="Net Sports Media"><img src="<?php bloginfo('stylesheet_directory'); ?>/imgs/custom-ados-themes/netsportsMedia_sm.jpg"></a>
              <h4>Net Sports Media</h4>
        </div>
 
