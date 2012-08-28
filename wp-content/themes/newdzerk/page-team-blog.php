755887560f9f56fed2fea96292c04bca165f276f
Jonathan Stephens <jonathanmstephens@gmail.com>
2012-08-28 10:14:52 -0400

    updated team blog category id for live site

--- wp-content/themes/newdzerk/page-team-blog.php
+++ wp-content/themes/newdzerk/page-team-blog.php
@@ -7,7 +7,7 @@
     </hgroup>
 
 
-<?php query_posts( 'cat=11'.'&paged='.$paged );?>
+<?php query_posts( 'cat=26'.'&paged='.$paged );?>
 
        <div class="blog-list-container d-all">
        	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
