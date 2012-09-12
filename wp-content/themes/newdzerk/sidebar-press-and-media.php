80f6299a4681b2dc15da5bae96f63f85837a9ec5
Sarah Kahn <skahn@adzerk.com>
2012-09-12 14:27:12 -0400

    populating logo links

--- wp-content/themes/newdzerk/sidebar-press-and-media.php
+++ wp-content/themes/newdzerk/sidebar-press-and-media.php
@@ -2,9 +2,9 @@
        <div class="press-kit">
               <h4>Need our logo? Get a nice vector version:</h4>
               <ul>
-                     <li>Blue gradient logo <a href="#">.png</a> | <a href="#">.eps</a></li>
-                     <li>Solid black logo <a href="#">.png</a> | <a href="#">.eps</a></li>
-                     <li>Solid white logo <a href="#">.png</a> | <a href="#">.eps</a></li>
+                     <li>Adzerk logo <a href="http://adzerk-www.s3.amazonaws.com/resources/adzerk-logo.png">.png</a> | <a href="http://adzerk-www.s3.amazonaws.com/adzerk-logo.ai">.ai</a></li>
+                     <li>adOS logo <a href="http://adzerk-www.s3.amazonaws.com/resources/adOs_logo_blue.png">.png</a> | <a href="http://adzerk-www.s3.amazonaws.com/adOS-final.pdf">.pdf</a></li>
+                     <li>Solid white logo adOS logo <a href="http://adzerk-www.s3.amazonaws.com/resources/adOS_logo_white.png">.png</a></li>
               </ul>            
        </div>
 
